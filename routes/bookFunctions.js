7174ce0c10591b104d56137c12dacef1822a1ba2
Yukinaa1q <144225410+Yukinaa1q@users.noreply.github.com>
2024-04-14 11:01:46 +0700

    Modify bookApis

--- routes/bookFunctions.js
+++ routes/bookFunctions.js
@@ -2,11 +2,11 @@ const router = require("express").Router();
 
 const Book = require("../models/book.js");
 
-const getNewID = (lastID) => {
-  const numericPart = parseInt(lastID.slice(2), 10);
-  const nextNumericPart = numericPart + 1;
-  return `LN${nextNumericPart.toString().padStart(5, "0")}`;
-};
+// const getNewID = (lastID) => {
+//   const numericPart = parseInt(lastID.slice(2), 10);
+//   const nextNumericPart = numericPart + 1;
+//   return `LN${nextNumericPart.toString().padStart(5, "0")}`;
+// };
 router.get("/genre", async (req, res) => {
   try {
     const boks = await Book.distinct("genre");
@@ -47,63 +47,63 @@ router.get("/:id", async (req, res) => {
     res.status(400).json({ error: error.message });
   }
 });
-router.post("/create", async (req, res) => {
-  const { size, genre, price, name, product_count, provider } = req.body;
+// router.post("/create", async (req, res) => {
+//   const { size, genre, price, name, product_count, provider } = req.body;
 
-  try {
-    const lastBok = await Book.findOne({}).sort({ id: -1 });
-    const newID = lastBok ? getNewID(lastBok.id) : "LN00001";
+//   try {
+//     const lastBok = await Book.findOne({}).sort({ id: -1 });
+//     const newID = lastBok ? getNewID(lastBok.id) : "LN00001";
 
-    const bok = await Book.create({
-      id: newID,
-      size,
-      genre,
-      price,
-      name,
-      product_count,
-      provider: newProd._id,
-    });
-    res.status(200).json(bok);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
-router.put("/:id", async (req, res) => {
-  const { id } = req.params;
-  const { name, genre, size, price, product_count, provider } = req.body;
-  try {
-    const bok = await Book.findOneAndUpdate(
-      { id },
-      {
-        $set: {
-          name,
-          genre,
-          size,
-          price,
-          product_count,
-          "provider.name": provider && provider.name,
-        },
-      }
-    );
-    if (!bok)
-      return res.status(404).json({ message: "There no such book exist" });
-    res.status(200).json(bok);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
-router.delete("/:id", async (req, res) => {
-  const { id } = req.params;
-  try {
-    const target = await Book.findOne({ id });
+//     const bok = await Book.create({
+//       id: newID,
+//       size,
+//       genre,
+//       price,
+//       name,
+//       product_count,
+//       provider: newProd._id,
+//     });
+//     res.status(200).json(bok);
+//   } catch (error) {
+//     res.status(400).json({ error: error.message });
+//   }
+// });
+// router.put("/:id", async (req, res) => {
+//   const { id } = req.params;
+//   const { name, genre, size, price, product_count, provider } = req.body;
+//   try {
+//     const bok = await Book.findOneAndUpdate(
+//       { id },
+//       {
+//         $set: {
+//           name,
+//           genre,
+//           size,
+//           price,
+//           product_count,
+//           "provider.name": provider && provider.name,
+//         },
+//       }
+//     );
+//     if (!bok)
+//       return res.status(404).json({ message: "There no such book exist" });
+//     res.status(200).json(bok);
+//   } catch (error) {
+//     res.status(400).json({ error: error.message });
+//   }
+// });
+// router.delete("/:id", async (req, res) => {
+//   const { id } = req.params;
+//   try {
+//     const target = await Book.findOne({ id });
 
-    const bok = await Book.findOneAndDelete({ id });
-    if (!bok) {
-      res.json({ message: "There is no book with this id exits" });
-    }
-    res.status(200).json(bok);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+//     const bok = await Book.findOneAndDelete({ id });
+//     if (!bok) {
+//       res.json({ message: "There is no book with this id exits" });
+//     }
+//     res.status(200).json(bok);
+//   } catch (error) {
+//     res.status(400).json({ error: error.message });
+//   }
+// });
 module.exports = router;
