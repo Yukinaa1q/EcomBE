f0abb728b2ec23c7f9e2a500516ecf16401b03a7
Yukinaa1q <144225410+Yukinaa1q@users.noreply.github.com>
2024-04-18 21:23:07 +0700

    modify delete api in user

--- routes/userFunctions.js
+++ routes/userFunctions.js
@@ -71,7 +71,7 @@ router.delete("/:id/:product_id", async (req, res) => {
 router.get("/:id/shipping", async (req, res) => {
   const { id } = req.params;
   try {
-    const result = await User.findOne({ id }).distinct("shipping");
+    const result = await User.findById(id).distinct("shipping");
     if (result) {
       res.status(200).json(result);
     } else {
